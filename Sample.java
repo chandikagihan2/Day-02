f78bfbf109abffd48ad58331d8557a362f9ca087
Chandikagihan2 <Chandikagihan2@gmail.com>
2026-09-23 11:36:29 +0530

    remove line 7

--- Sample.java
+++ Sample.java
@@ -1,12 +1,13 @@
 import java.util.*;
+
 class Sample {
 
-    public static void printName( ){
+    public static void printName() {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter your name: ");
         String name = input.nextLine();
-        System.out.println("I am chandika, i am a student at ICET.");
     }
+
     public static void main(String[] args) {
         System.out.println("Hello, Everyone!");
         System.out.println("I am chandika, i am a student at ICET.");
