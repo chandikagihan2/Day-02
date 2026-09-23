710e4f698c21532d2e77277782b5ad1e28eb1619
Chandikagihan2 <Chandikagihan2@gmail.com>
2026-09-23 11:27:01 +0530

    change all detail

--- Sample.java
+++ Sample.java
@@ -1,14 +1,14 @@
+import java.util.*;
 class Sample {
 
-    public ststic void printName( Name, NamespaceContext)(String[] args) {
-        System.out.println("Hello, Everyone!");
+    public static void printName( ){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = input.nextLine();
         System.out.println("I am chandika, i am a student at ICET.");
     }
     public static void main(String[] args) {
         System.out.println("Hello, Everyone!");
-        System.out.println("I am chandika, i am a student at ICET.");
-    }
-}
         System.out.println("I am chandika, i am a student at ICET.");
     }
 }
