055475e2ab8d3d344aead378793fce0669f0a5be
Chandikagihan2 <Chandikagihan2@gmail.com>
2026-09-23 11:22:28 +0530

    input data

--- Sample.java
+++ Sample.java
@@ -1,6 +1,14 @@
 class Sample {
+
+    public ststic void printName( Name, NamespaceContext)(String[] args) {
+        System.out.println("Hello, Everyone!");
+        System.out.println("I am chandika, i am a student at ICET.");
+    }
     public static void main(String[] args) {
         System.out.println("Hello, Everyone!");
+        System.out.println("I am chandika, i am a student at ICET.");
+    }
+}
         System.out.println("I am chandika, i am a student at ICET.");
     }
 }
