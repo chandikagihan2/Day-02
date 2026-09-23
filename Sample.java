7ee5e721fb5b01bb8e62a732427418c00a442613
Chandikagihan2 <Chandikagihan2@gmail.com>
2026-09-23 11:14:52 +0530

    adding ,method

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 class Sample {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("hello lovely");
+        System.out.println("Hello, Everyone!");
+        System.out.println("I am chandika, i am a student at ICET.");
     }
 }
