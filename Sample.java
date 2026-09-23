e5bf84e785ffdd68e33d0179289e141e4f1915db
Chandikagihan2 <Chandikagihan2@gmail.com>
2026-09-23 11:37:52 +0530

    prinlName method

--- Sample.java
+++ Sample.java
@@ -10,6 +10,6 @@ class Sample {
 
     public static void main(String[] args) {
         System.out.println("Hello, Everyone!");
-        System.out.println("I am chandika, i am a student at ICET.");
+        printName();
     }
 }
